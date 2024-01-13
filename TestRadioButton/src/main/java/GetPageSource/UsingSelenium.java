2a4e51f68195ef7b73097b3837c4c370695e337e
thecong <congthuanthanh1@gmail.com>
2024-01-13 01:51:48 -0800

    add package GetPageSource

---
+++ TestRadioButton/src/main/java/GetPageSource/UsingSelenium.java
@@ -0,0 +1,43 @@
+package GetPageSource;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class UsingSelenium {
+    public static String getHtmlContent(String linkHtml) {
+//    System.setProperty("Webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(linkHtml);
+//        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+//
+//        String htmlContent = driver.getPageSource();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+        System.out.println(wait.until(ExpectedConditions.jsReturnsValue("return document.readyState;")).toString());
+        String htmlContent = driver.getPageSource();
+        driver.quit();
+        return htmlContent;
+    }
+
+    public static void writeHtmlContentToFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("E:\\LAB UI\\TestFindAndSelectElement\\TestRadioButton\\src\\main\\resources\\index.html");
+        fileWriter.write(content);
+        fileWriter.close();
+    }
+
+    public static void main(String[] args) {
+        String linkHtml = "https://form.jotform.com/233591282365460";
+        String content = getHtmlContent(linkHtml);
+        try {
+            writeHtmlContentToFile(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
